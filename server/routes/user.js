bf46bfc6e4ac7d8068eae35bbf79155803736dd6
Phembarl <>
2019-04-03 11:44:37 +0100

    chore(ui templates) adjust templates - enable user experience responsive interface [Finishes #165084334]

--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import user from '../controllers/user';
-import validator from '../middleware/validator';
-
-const router = express.Router();
-
-router.post('/auth/signup', validator, user.createUser);
-
-export default router;
