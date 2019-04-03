e9db4e850e54b3941cb8405bbdba3faabb08214e
Phembarl <>
2019-04-03 01:35:09 +0100

    feature(api endpoints): create debit account api - enable staff (cashier) make api POST request to debit user account [Finishes #164882492]

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import transaction from '../controllers/credit';
-import validCredit from '../middleware/creditValidator';
-// import newAccountValidator from '../middleware/newAccountValidator';
+import transaction from '../controllers/transactions';
+import validTransaction from '../middleware/transactionValidator';
 
 const transactionsRouter = express.Router();
 
 transactionsRouter.get('/transactions', transaction.getTransactions);
-transactionsRouter.post('/transactions/:accountNumber/credit', validCredit, transaction.credit);
+transactionsRouter.post('/transactions/:accountNumber/credit', validTransaction, transaction.credit);
+transactionsRouter.post('/transactions/:accountNumber/debit', validTransaction, transaction.debit);
+
 export default transactionsRouter;
